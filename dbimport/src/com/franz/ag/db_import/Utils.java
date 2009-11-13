95d02878d4f3570d4cd812757c01259cf343c340
Mike Hinchey <mhinchey@franz.com>
2009-11-13 14:58:42 -0800

    agraph32: delete files not released with agraph32

--- dbimport/src/com/franz/ag/db_import/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.franz.ag.db_import;
-
-public class Utils {
-	
-	public static boolean isNullString (String s) {
-		return (s == null) || ("".equals(s));
-	}
-
-}
--- httpdsail/src/org/openrdf/repository/sail/CompoundLiteral.java
+++
@@ -1,7 +0,0 @@
-package org.openrdf.repository.sail;
-
-import org.openrdf.model.impl.LiteralImpl;
-
-public class CompoundLiteral extends LiteralImpl {
-
-}
