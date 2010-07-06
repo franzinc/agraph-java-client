704454c53bd8580aacd882048f3db785eb94c343
Mike Hinchey <mhinchey@franz.com>
2010-07-06 16:26:21 -0700

    bug19485: setAutoCommit(true) causes exception
    Tests added for Jena autoCommit true and false. prepush passes.
    
    <release-note>
    bug19485: setAutoCommit(true) causes exception
    
    True is the default, but calling this would raise an exception, so
    this has been fixed.
    </release-note>
    
    Change-Id: I9192fb6575968d8ef017bb0b199b629dbe208a63

---
+++ src/test/Closer.java
@@ -0,0 +1,97 @@
+package test;
+
+import info.aduna.iteration.CloseableIteration;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.franz.util.Closeable;
+
+/**
+ * Extend this class to add easy ability to safely close various resources.
+ * 
+ * TODO: move class to com.franz.util.
+ */
+public abstract class Closer implements Closeable {
+
+	private final List toClose = new LinkedList();
+
+	/**
+	 * Add a resource to be closed with {@link #close()}.
+	 */
+    public <CloseableType extends Closeable>
+    CloseableType closeLater(CloseableType o) {
+		toClose.add(o);
+		return o;
+	}
+
+	/**
+	 * Add a resource to be closed with {@link #close()}.
+	 */
+	public <CloseIter extends CloseableIteration>
+	CloseIter closeLater(CloseIter o) {
+		toClose.add(o);
+		return o;
+    }
+
+	/**
+	 * Add a resource to be closed with {@link #close()}.
+	 */
+    public <CloseableType extends java.io.Closeable>
+    CloseableType closeLater(CloseableType o) {
+		toClose.add(o);
+		return o;
+    }
+
+	/**
+	 * Must be called in a finally block, to close all resources
+	 * added with closeLater().
+	 */
+	public void close() {
+		while (toClose.isEmpty() == false) {
+			Object o = toClose.get(0);
+			close(o);
+			while (toClose.remove(o)) {
+			}
+		}
+	}
+
+	/**
+	 * TODO: move to com.franz.util.Util
+	 */
+	public static <Elem extends Object, Exc extends Exception>
+	CloseableIteration<Elem, Exc> close(CloseableIteration<Elem, Exc> o) {
+        if (o != null) {
+            try {
+                o.close();
+            } catch (Exception e) {
+                System.err.println("ignoring error with close:" + e);
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+	/**
+	 * TODO: move to com.franz.util.Util
+	 */
+    public static <Obj extends Object>
+    Obj close(Obj o) {
+        if (o instanceof Closeable) {
+            com.franz.util.Util.close((Closeable)o);
+        } else if (o instanceof java.io.Closeable) {
+            com.franz.util.Util.close((java.io.Closeable)o);
+        } else if (o instanceof CloseableIteration) {
+        	close((CloseableIteration)o);
+        } else if (o != null) {
+            try {
+                o.getClass().getMethod("close").invoke(o);
+            } catch (Exception e) {
+                System.err.println("ignoring error with close:" + e);
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    
+}
