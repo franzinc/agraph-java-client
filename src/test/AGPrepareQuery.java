8d0f601885396c650484d247d97e25a3a8cf6b96
Bill Millar <bmillar@franz.com>
2010-08-06 16:54:35 -0700

    bug19569: java client now helps delete orphaned saved queries
    Preparing a Sesame AGQuery currently has the side effect of saving the
    prepared query on the server in memory for the duration of the session.
    A client app that prepares many queries during a session thus uses up
    server memory.  If an AGQuery instance goes out of scope, the saved
    query is no longer needed on the server and is currently only deleted
    when the session expires.  With this change, the client library no
    longer uses the saved query service during a prepare query, pending
    further cost-benefit analysis of the saved query service.  Client code
    can explicitly request that a query be saved using an experimental
    AGQuery#setSaveName method.  The client library also tries to help
    delete orphaned saved queries sooner by tracking when AGQuery instances
    are garbage collected on the client and issuing delete requests for the
    orphaned saved queries in subsequent query requests.
    
    Test: see AGPrepareQuery.java
    make prepush run
    ant tutorial run
    ant jenatutorial run
    
    <release-note>
    bug19569: java client now helps delete orphaned saved queries
    
    Preparing a Sesame AGQuery currently has the side effect of saving the
    prepared query on the server in memory for the duration of the session.
    A client app that prepares many queries during a session thus uses up
    server memory.  If an AGQuery instance goes out of scope, the saved
    query is no longer needed on the server and is currently only deleted
    when the session expires.  With this change, the client library no
    longer uses the saved query service during a prepare query, pending
    further cost-benefit analysis of the saved query service.  Client code
    can explicitly request that a query be saved using an experimental
    AGQuery#setSaveName method.  The client library also tries to help
    delete orphaned saved queries sooner by tracking when AGQuery instances
    are garbage collected on the client and issuing delete requests for the
    orphaned saved queries in subsequent query requests.
    </release-note>
    
    Change-Id: Id3ff3ab679561d35ff78717ca1001cc4cbe29f0a

---
+++ src/test/AGPrepareQuery.java
@@ -0,0 +1,76 @@
+/******************************************************************************
+** Copyright (c) 2008-2010 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+
+package test;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+
+import tutorial.TutorialExamples;
+
+import com.franz.agraph.repository.AGRepositoryConnection;
+import com.franz.agraph.repository.AGTupleQuery;
+
+public class AGPrepareQuery extends TutorialExamples {
+
+	/**
+	 * Exercises prepared and unprepared queries, and occasionally 
+	 * forces gc of old AGQuery instances so that orphaned saved 
+	 * queries can be deleted during subsequent prepare requests; 
+	 * verify by watching the http traffic to the server.
+	 *   
+	 * @throws Exception
+	 */
+	public static void prepareQueryExample() throws Exception {
+		AGRepositoryConnection conn = example2(false);
+		ValueFactory f = conn.getValueFactory();
+		conn.setAutoCommit(false);
+		URI alice = f.createURI("http://example.org/people/alice");
+		URI bob = f.createURI("http://example.org/people/bob");
+		String queryString = "select ?s ?p ?o where { ?s ?p ?o} ";
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+			AGTupleQuery tupleQuery = conn.prepareTupleQuery(
+					QueryLanguage.SPARQL, queryString);
+			tupleQuery.setSaveName(String.valueOf(10*i+j));
+			tupleQuery.setBinding("s", alice);
+			TupleQueryResult result = tupleQuery.evaluate();
+			println("\nFacts about Alice:");
+			while (result.hasNext()) {
+				println(result.next());
+			}
+			result.close();
+			tupleQuery.setBinding("s", bob);
+			println("\nFacts about Bob:");
+			result = tupleQuery.evaluate();
+			while (result.hasNext()) {
+				println(result.next());
+			}
+			result.close();
+			TupleQuery tupleQuery2 = new AGTupleQuery(conn, QueryLanguage.SPARQL, queryString, null);
+			result = tupleQuery2.evaluate();
+			println("\nAll Facts:");
+			while (result.hasNext()) {
+				println(result.next());
+			}
+			result.close();
+			}
+			System.gc();
+		}
+		conn.close();
+		System.gc();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		prepareQueryExample();
+ 	}
+
+}
