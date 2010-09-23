02e4f84723d4eb0bd699db625aaed2bf66f5170c
Kevin Layer <layer@franz.com>
2010-09-22 18:14:12 -0700

    synchronize between phases of the events test
    The synchronization depends on variables setup by scripts in
    the agraph-bench repo (master branch).  With these variables the new
    changes are a no-op.
    
    Change-Id: Ib935eb88fc219cb2826206734fa89f136acb4a5f

--- src/test/stress/monitor.sh
+++ src/test/stress/monitor.sh
@@ -8,4 +8,17 @@ elif [ "$1" = end ]
 then
     echo "monitor.sh: Ending monitoring."
     killall -USR2 nmon
+
+    dbname=events_test
+    catalog=tests
+
+    [ -n "${AGRAPH_LISP_CLIENT-}" ] &&
+    [ -n "${AGRAPH_BENCH_DIR-}" ] &&
+    [ -n "${AGRAPH_PORT-}" ] &&
+    /fi/cl/8.2/agraph/bin/mlisp-64 \
+	-L $AGRAPH_LISP_CLIENT/agraph4.fasl \
+	-L $AGRAPH_BENCH_DIR/ensure-db-idle.cl \
+	-e "(db.agraph.storage::synchronize \
+               \"${dbname}\" \"${catalog}\" $AGRAPH_PORT)" \
+	-kill
 fi
