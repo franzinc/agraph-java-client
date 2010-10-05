a4d78225342ad1486345e6761c7caa38ab538977
Bill Millar <bmillar@franz.com>
2010-10-05 15:40:27 -0700

    rfe10119: add analyze() method to provide query analysis
    <release-note>
    rfe10119: add analyze() method to provide query analysis
    
    Sesame AGQuery subclasses can now call the analyze() method to obtain a
    query analysis for the query rather than evaluating it.
    </release-note>
    
    prepush tests added to TutorialTests for SPARQL SELECT queries.
    make prepush passes
    ant tutorial runs
    
    Change-Id: I103678a07017df9233b4d0dfad6b301f540b89e1

--- src/com/franz/agraph/repository/AGGraphQuery.java
+++ src/com/franz/agraph/repository/AGGraphQuery.java
@@ -8,19 +8,14 @@
 
 package com.franz.agraph.repository;
 
-import java.io.IOException;
-
-import org.apache.commons.httpclient.HttpException;
 import org.openrdf.query.GraphQuery;
 import org.openrdf.query.GraphQueryResult;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.impl.GraphQueryResultImpl;
-import org.openrdf.repository.RepositoryException;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandler;
 import org.openrdf.rio.RDFHandlerException;
-import org.openrdf.rio.RDFParseException;
 import org.openrdf.rio.helpers.StatementCollector;
 
 import com.franz.agraph.http.AGResponseHandler;
@@ -65,20 +60,9 @@ public class AGGraphQuery extends AGQuery implements GraphQuery {
 	 */
 	public void evaluate(RDFHandler handler) throws QueryEvaluationException,
 			RDFHandlerException {
-		try {
-			// TODO: deal with the hard coded return format
-			httpCon.getHttpRepoClient().query(this,
-					new AGResponseHandler(httpCon.getRepository(), handler,
+		// TODO: deal with the hard coded return format
+		evaluate(new AGResponseHandler(httpCon.getRepository(), handler,
 							RDFFormat.NTRIPLES));
-		} catch (HttpException e) {
-			throw new QueryEvaluationException(e);
-		} catch (RepositoryException e) {
-		    throw new QueryEvaluationException(e);
-		} catch (RDFParseException e) {
-		    throw new QueryEvaluationException(e);
-		} catch (IOException e) {
-		    throw new QueryEvaluationException(e);
-		}
 	}
 
 }
