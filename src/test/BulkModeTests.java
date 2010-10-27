c5ba1ba9edb66a1951a7ca0c3aab54d12a6d72f6
Bill Millar <bmillar@franz.com>
2010-10-27 09:54:50 -0700

    rfe10143: support bulkMode in connections
    <release-note>
    rfe10143: support bulkMode in connections
    
    With this change, connections can now be put into bulkMode.
    When in bulkMode, data can be added/loaded more quickly, but
    there is no guarantee of durability in the event of a crash.
    Added method AGRepositoryConnection#setBulkMode to set the
    connection's bulkMode (defaults to false), and added method
    AGRepositoryConnection#isBulkMode to check the mode.
    <release-note>
    
    Added BulkModeTests to prepush tests
    make prepush passes
    ant tutorial runs
    
    Change-Id: I5b134555e727e56895d376dbb293ad176b9bd2e0
    Reviewed-on: https://gerrit.franz.com:9080/948
    Reviewed-by: Kevin Layer <layer@franz.com>
    Tested-by: Kevin Layer <layer@franz.com>

---
+++ src/test/BulkModeTests.java
@@ -0,0 +1,49 @@
+/******************************************************************************
+** Copyright (c) 2008-2010 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openrdf.model.Resource;
+import org.openrdf.model.URI;
+import org.openrdf.rio.RDFFormat;
+
+public class BulkModeTests extends AGAbstractTest {
+
+    @Test
+    @Category(TestSuites.Prepush.class)
+    public void bulkMode_rfe10143() throws Exception {
+    	Assert.assertTrue("expected autoCommit true", conn.isAutoCommit());
+    	Assert.assertFalse("expected bulkMode false", conn.isBulkMode());
+    	conn.setBulkMode(true);
+    	Assert.assertTrue("expected autoCommit true", conn.isAutoCommit());
+    	Assert.assertTrue("expected bulkMode true", conn.isBulkMode());
+        String path1 = "src/tutorial/java-vcards.rdf";    
+        URI context = vf.createURI("http://example.org#vcards");
+        conn.add(new File(path1), null, RDFFormat.RDFXML, context);
+        assertEquals("expected 16 vcard triples", 16, conn.size(context));
+        conn.setAutoCommit(false);
+        Assert.assertFalse("expected autoCommit false", conn.isAutoCommit());
+        Assert.assertTrue("expected bulkMode true", conn.isBulkMode());
+        String path2 = "src/tutorial/java-kennedy.ntriples";            
+        conn.add(new File(path2), null, RDFFormat.NTRIPLES);
+        assertEquals("expected 1214 kennedy triples", 1214, conn.size((Resource)null));
+        assertEquals("expected 1230 total triples", 1230, conn.size());
+        conn.rollback();
+        assertEquals("expected 0 kennedy triples", 0, conn.size((Resource)null));
+        assertEquals("expected 16 total triples", 16, conn.size());
+   }
+
+}
