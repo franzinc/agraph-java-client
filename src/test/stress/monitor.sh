5d661e0f9b3bab4a2cbdea41b56b3ed7b743d0cb
Mikel Bancroft <mikel@franz.com>
2010-12-01 09:52:23 -0800

    bug19881. conditionalize sync phase. pass catalog and db to monitor.sh
    src/test/stress/Events.java:
    - pass catalog and repository to invocation of monitor.sh
    
    src/test/stress/monitor.sh:
    - conditionalize sync phases, so they don't trigger unless
      /fi/cl/8.2/bin/mlisp-64 exists. Needed so events tests don't
      fail at customer sites.
    
    - monitor.sh no longer uses a hardwired dbname and catalog, and
      accepts 4 command-line arguments. It previously accepted 1 or 2
      depending on invocation.
    
      1. "start" or "stop"
      2. a string describing the phase
      3. a string naming a catalog
      4. a string naming a repository.
    
    Tests added for: none
    make test-suite run? no
    
    release notes: non-user-visible changes.
    
    Change-Id: I4fef0f4c9d61e6524d692ff9b0dd28ca8985373b
    Reviewed-on: https://gerrit.franz.com:9080/1024
    Reviewed-by: Kevin Layer <layer@franz.com>
    Tested-by: Kevin Layer <layer@franz.com>

--- src/test/stress/monitor.sh
+++ src/test/stress/monitor.sh
@@ -7,19 +7,20 @@ elif test "$1" = end; then
     echo "monitor.sh: Ending monitoring..."
     killall -USR2 nmon
 
-    dbname=events_test
-    catalog=tests
+    catalog=$3
+    dbname=$4
 
     [ -n "${AGRAPH_LISP_CLIENT-}" ] &&
     [ -n "${AGRAPH_BENCH_DIR-}" ] &&
     [ -n "${AGRAPH_PORT-}" ] &&
+    [ -f /fi/cl/8.2/agraph/bin/mlisp-64 ] &&
     echo "[" `date --rfc-3339=ns` "] Phase 0 Begin: (sync)" &&
     /fi/cl/8.2/agraph/bin/mlisp-64 \
 	-L $AGRAPH_LISP_CLIENT/agraph4.fasl \
 	-L $AGRAPH_BENCH_DIR/ensure-db-idle.cl \
 	-e "(db.agraph.storage::synchronize \
                \"${dbname}\" \"${catalog}\" $AGRAPH_PORT)" \
-	-kill
+	-kill &&
     echo "[" `date --rfc-3339=ns` "] Phase 0 End: (sync)."
 else
     echo $0: unknown first argument: $1
