2c468671c4bf7249edb041d4a3b3f6a2029fd988
Kevin Layer <layer@franz.com>
2011-02-10 11:57:48 -0800

    Adjusted synchronization process
    [Mirrors changes Ahmon made for Python]
    
    Changed the call to the synchronization function to correspond to
    changes made in the agraph module.
    
    Change-Id: Ibe16e3af270dc7996db0df433274fb8d4312675b

--- src/test/stress/monitor.sh
+++ src/test/stress/monitor.sh
@@ -4,22 +4,19 @@ if test "$1" = start; then
     echo "monitor.sh: Starting monitoring for $2..."
     nmon -t -C agraph -F "$2.nmon" -s 10
 elif test "$1" = end; then
-    echo "monitor.sh: Ending monitoring..."
+    echo "monitor.sh: Ending monitoring."
     killall -USR2 nmon
 
     catalog=$3
     dbname=$4
 
     [ -n "${AGRAPH_LISP_CLIENT-}" ] &&
-    [ -n "${AGRAPH_BENCH_DIR-}" ] &&
-    [ -n "${AGRAPH_PORT-}" ] &&
     [ -f /fi/cl/8.2/agraph/bin/mlisp-64 ] &&
     echo "[" `date --rfc-3339=ns` "] Phase 0 Begin: (sync)" &&
     /fi/cl/8.2/agraph/bin/mlisp-64 \
 	-L $AGRAPH_LISP_CLIENT/agraph4.fasl \
-	-L $AGRAPH_BENCH_DIR/ensure-db-idle.cl \
-	-e "(db.agraph.storage::synchronize \
-               \"${dbname}\" \"${catalog}\" $AGRAPH_PORT)" \
+	-e "(db.agraph.storage::sl-ensure-db-idle \
+               \"${dbname}\" \"${catalog}\" ${AGRAPH_PORT:-nil})" \
 	-kill &&
     echo "[" `date --rfc-3339=ns` "] Phase 0 End: (sync)."
 else
