52b755d672cbd11233838b75c1c7f32aac310c6c
Bill Millar <bmillar@franz.com>
2011-03-04 15:24:01 -0800

    rfe10448: update copyright headers to 2011
    With this change, source files in the agraph-java-client repo
    have their copyright headers updated to 2011.
    
    make prepush passes.
    
    No release note needed.
    
    Change-Id: Ib83d27030b473ccb30dc72346d6e382342ab256c

--- clojure/test/com/franz/agraph/agtest.clj
+++ clojure/test/com/franz/agraph/agtest.clj
@@ -1,5 +1,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Copyright (c) 2008-2010 Franz Inc.
+;; Copyright (c) 2008-2011 Franz Inc.
 ;; All rights reserved. This program and the accompanying materials
 ;; are made available under the terms of the Eclipse Public License v1.0
 ;; which accompanies this distribution, and is available at
