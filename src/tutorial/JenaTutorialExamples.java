1d5ef30fba49a60b50ceb6526a996507d50ccab1
Bill Millar <bmillar@franz.com>
2011-03-25 16:01:04 -0700

    rfe10450: Jena Prolog and transaction code examples
    <release-note>
    rfe10450: Jena Prolog and transaction code examples
    
    With this change, Jena Prolog and transaction examples are now
    available in the distribution in JenaTutorialExamples.java as
    examples 17, 18, and 22; they will eventually also be written
    up in the Jena tutorial document, but the focus here is on making
    the code examples available as they are useful on their own.
    <release-note>
    
    make prepush passes
    ant jena-tutorial runs
    
    Change-Id: I3507b92579728b3b98e850babf12f171d82070ff

--- src/tutorial/JenaTutorialExamples.java
+++ src/tutorial/JenaTutorialExamples.java
@@ -23,6 +23,7 @@ import com.franz.agraph.jena.AGQueryExecutionFactory;
 import com.franz.agraph.jena.AGQueryFactory;
 import com.franz.agraph.jena.AGReasoner;
 import com.franz.agraph.repository.AGCatalog;
+import com.franz.agraph.repository.AGQueryLanguage;
 import com.franz.agraph.repository.AGRepository;
 import com.franz.agraph.repository.AGRepositoryConnection;
 import com.franz.agraph.repository.AGServer;
@@ -560,6 +561,85 @@ public class JenaTutorialExamples {
 			qe.close();
 		}
 	}
+    /**
+     * Prolog queries
+     */
+    public static void example17() throws Exception {
+        AGGraphMaker maker = example6();
+        AGGraph graph = maker.getGraph();
+        AGModel model = new AGModel(graph);
+        try {
+        model.setNsPrefix("kdy", "http://www.franz.com/simple#");
+        String rules1 =
+            "(<-- (woman ?person) ;; IF\n" +
+            "     (q ?person !kdy:sex !kdy:female)\n" +
+            "     (q ?person !rdf:type !kdy:person))\n" +
+            "(<-- (man ?person) ;; IF\n" +
+            "     (q ?person !kdy:sex !kdy:male)\n" +
+            "     (q ?person !rdf:type !kdy:person))";
+        maker.getRepositoryConnection().addRules(rules1);
+        println("\nFirst and Last names of all \"men.\"");
+        String queryString =
+            "(select (?first ?last)\n" +
+            "        (man ?person)\n" +
+            "        (q ?person !kdy:first-name ?first)\n" +
+            "        (q ?person !kdy:last-name ?last))";
+			AGQuery prolog = AGQueryFactory.create(AGQueryLanguage.PROLOG,queryString);
+			QueryExecution qe = AGQueryExecutionFactory.create(prolog, model);
+			try {
+				ResultSet results = qe.execSelect();
+				while (results.hasNext()) {
+					QuerySolution result = results.next();
+					RDFNode f = result.get("first");
+					RDFNode l = result.get("last");
+					System.out.println(f + " " + l);
+				}
+			} finally {
+				qe.close();
+			}
+		} finally {
+			model.close();
+		}
+    }
+
+	/**
+	 * Loading Prolog rules
+	 */
+	public static void example18() throws Exception {
+		AGGraphMaker maker = example6();
+		AGGraph graph = maker.createGraph("http://example18.org");
+		AGModel model = new AGModel(graph);
+		try {
+			model.setNsPrefix("kdy", "http://www.franz.com/simple#");
+			model.setNsPrefix("rltv", "http://www.franz.com/simple#");
+			String path = "src/tutorial/java-rules.txt";
+			maker.getRepositoryConnection().addRules(new FileInputStream(path));
+			String queryString = "(select (?ufirst ?ulast ?cfirst ?clast)"
+					+ "(uncle ?uncle ?child)" + "(name ?uncle ?ufirst ?ulast)"
+					+ "(name ?child ?cfirst ?clast))";
+			AGQuery prolog = AGQueryFactory.create(AGQueryLanguage.PROLOG,
+					queryString);
+			QueryExecution qe = AGQueryExecutionFactory.create(prolog, model);
+			try {
+				ResultSet results = qe.execSelect();
+				while (results.hasNext()) {
+					QuerySolution result = results.next();
+					RDFNode ufirst = result.get("ufirst");
+					RDFNode ulast = result.get("ulast");
+					String ufull = ufirst + " " + ulast;
+					RDFNode cfirst = result.get("cfirst");
+					RDFNode clast = result.get("clast");
+					String cfull = cfirst + " " + clast;
+					println(ufull + " is the uncle of " + cfull);
+				}
+			} finally {
+				qe.close();
+			}
+		} finally {
+			model.close();
+		}
+	}
+
 	/**
 	 * RDFS++ Reasoning
 	 */
@@ -653,8 +733,76 @@ public class JenaTutorialExamples {
 		printRows(infmodel.listStatements(null, RDF.type, child));
 	}
 
-	/**
-	 * Usage: all Usage: [1-9,11,13,19]+
+    /**
+     * Transactions
+     */
+    public static void example22() throws Exception {
+    	AGGraphMaker maker1 = example1(false);
+    	// Get another graph maker for the repository on a another connection
+    	AGRepositoryConnection conn2 = maker1.getRepositoryConnection().getRepository().getConnection();
+    	closeBeforeExit(conn2);
+    	AGGraphMaker maker2 = new AGGraphMaker(conn2);
+        AGModel model1 = new AGModel(maker1.getGraph());
+        AGModel model2 = new AGModel(maker2.getGraph());
+        model2.begin();
+        String baseURI = "http://example.org/example/local";
+        model1.read(new FileInputStream("src/tutorial/java-lesmis.rdf"), baseURI);
+        println("Loaded " + model1.size() + " java-lesmis.rdf triples via conn1.");
+        model2.read(new FileInputStream("src/tutorial/java-kennedy.ntriples"), baseURI, "N-TRIPLE");
+        println("Loaded " + model2.size() + " java-kennedy.ntriples via conn2.");
+        
+        println("\nSince model1 is not in a transaction, java-lesmis.rdf triples are committed " +
+        		"and retrievable via model2.  Since model2 is in a transaction, and " +
+        		"no commit() has yet been issued on model2, kennedy.rdf triples are not " +
+        		" retrievable via model1.");
+        // Check transaction isolation semantics:
+        Literal valjean = model1.createLiteral("Valjean");
+        Literal kennedy = model1.createLiteral("Kennedy");
+        printRows("\nUsing listStatements() on model1; should find Valjean:",
+                1, model1.listStatements(null, null, valjean));
+        printRows("\nUsing listStatements() on model1; should not find Kennedy:",
+                1, model1.listStatements(null, null, kennedy));
+        printRows("\nUsing listStatements() on model2; should not find Valjean (until a rollback or commit occurs on model2):",
+                1, model2.listStatements(null, null, valjean));
+        printRows("\nUsing listStatements() on model2; should find Kennedy:",
+                1, model2.listStatements(null, null, kennedy));
+        
+        // Rollback
+        println("\nRolling back contents of model2.");
+        model2.abort();
+        println("There are now " + model2.size() + " triples visible via model2.");
+        printRows("\nUsing listStatements() on model1; should find Valjean:",
+                1, model1.listStatements(null, null, valjean));
+        printRows("\nUsing listStatements() on model1; should not find Kennedys:",
+                1, model1.listStatements(null, null, kennedy));
+        printRows("\nUsing listStatements() on model2; should not find Kennedys:",
+                1, model2.listStatements(null, null, kennedy));
+        printRows("\nUsing listStatements() on model2; should find Valjean:",
+                1, model2.listStatements(null, null, valjean));
+        // Reload and Commit
+        println("\nReload java-kennedy.ntriples into model2.");
+        model2.begin();
+        model2.read(new FileInputStream("src/tutorial/java-kennedy.ntriples"), baseURI, "N-TRIPLE");
+        println("There are now " + model1.size() + " triples visible on model1.");
+        println("There are now " + model2.size() + " triples visible on model2.");
+        println("\nCommitting contents of model2.");
+        model2.commit();
+        println("There are now " + model1.size() + " triples visible on model1.");
+        println("There are now " + model2.size() + " triples visible on model2.");
+        printRows("\nUsing listStatements() on model1; should find Valjean:",
+                1, model1.listStatements(null, null, valjean));
+        printRows("\nUsing listStatements() on model1; should find Kennedys:",
+                1, model1.listStatements(null, null, kennedy));
+        printRows("\nUsing listStatements() on model2; should find Kennedys:",
+                1, model2.listStatements(null, null, kennedy));
+        printRows("\nUsing listStatements() on model2; should find Valjean:",
+                1, model2.listStatements(null, null, valjean));
+        maker1.close();
+        maker2.close();
+    }
+
+    /**
+	 * Usage: all Usage: [1-9,11,13,17-19,22]+
 	 */
 	public static void main(String[] args) throws Exception {
 		List<Integer> choices = new ArrayList<Integer>();
@@ -667,7 +815,10 @@ public class JenaTutorialExamples {
 			}
 			choices.add(11);
 			choices.add(13);
+			choices.add(17);
+			choices.add(18);
 			choices.add(19);
+			choices.add(22);
 		} else {
 			for (int i = 0; i < args.length; i++) {
 				choices.add(Integer.parseInt(args[i]));
@@ -710,8 +861,17 @@ public class JenaTutorialExamples {
 				case 13:
 					example13();
 					break;
+				case 17:
+					example17();
+					break;
+				case 18:
+					example18();
+					break;
 				case 19:
 					example19();
+					break;
+				case 22:
+					example22();
 					break;
 				default:
 					throw new IllegalArgumentException("There is no example "
@@ -773,4 +933,3 @@ public class JenaTutorialExamples {
 	}
 
 }
-//Update July 9, 2010 AG 4.1
