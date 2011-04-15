722529dab4e7d621f69f8e4b620a3eb9017c0d3c
Bill Millar <bmillar@franz.com>
2011-04-14 21:15:43 -0700

    rfe10452: support N-Quads responses in the Java client
    <release-note>
    rfe10452: support N-Quads responses in the Java client
    
    With this change, the Java client now supports N-Quads responses from the
    server.  The property -Dcom.franz.agraph.http.defaultRDFFormat=NQUADS can
    now be used to configure the client to request N-Quads format by default
    for RDF statement responses; when this property is not present, the default
    format is TriX.  The method AGHttpRepoClient#setPreferredRDFFormat() can
    also be used to override the default RDFFormat on a per connection basis.
    </release-note>
    
    'make prepush' passes (default still uses TriX)
    'make -Dcom.franz.agraph.http.defaultRDFFormat=NQUADS prepush' passes
    'ant tutorial' runs (default still using TriX)
    'ant  -Dcom.franz.agraph.http.defaultRDFFormat=NQUADS tutorial' runs
    A small events test passes with both TriX and NQUADS, using options:
    --catalog java-tutorial --load 2 --query 2 --time 1 --size 10000
    'make javadoc' runs clean
    
    Performance:
    
    The default RDFFormat of the client is still TriX.  When using the
    option -Dcom.franz.agraph.http.defaultRDFFormat=NQUADS with make
    prepush, ant tutorial, and a small events test, the performance is
    comparable to marginally faster; if the performance on larger tests
    is also comparable or faster, the default may be changed to NQUADS
    in future.
    
    Change-Id: I3d22315f5e3b12f7e7d901ee99cdf110a647b5a7

---
+++ src/com/franz/openrdf/rio/nquads/NQuadsParserFactory.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+** Copyright (c) 2008-2011 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+package com.franz.openrdf.rio.nquads;
+
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.RDFParserFactory;
+
+/**
+ * An {@link RDFParserFactory} for N-Quads parsers.
+ * 
+ */
+public class NQuadsParserFactory implements RDFParserFactory {
+
+	/**
+	 * Returns {@link NQuadsFormat#NQUADS}.
+	 */
+	public RDFFormat getRDFFormat() {
+		return NQuadsFormat.NQUADS;
+	}
+
+	/**
+	 * Returns a new instance of NQuadsParser.
+	 */
+	public RDFParser getParser() {
+		return new NQuadsParser();
+	}
+}
---
+++ src/com/franz/openrdf/rio/nquads/NQuadsWriterFactory.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+** Copyright (c) 2008-2011 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+package com.franz.openrdf.rio.nquads;
+
+import java.io.OutputStream;
+import java.io.Writer;
+
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.RDFWriterFactory;
+
+/**
+ * An {@link RDFWriterFactory} for N-Quads writers.
+ * 
+ */
+public class NQuadsWriterFactory implements RDFWriterFactory {
+
+	/**
+	 * Returns {@link NQuadsFormat#NQUADS}.
+	 */
+	public RDFFormat getRDFFormat() {
+		return NQuadsFormat.NQUADS;
+	}
+
+	/**
+	 * Returns a new instance of {@link NQuadsWriter}.
+	 */
+	public RDFWriter getWriter(OutputStream out) {
+		return new NQuadsWriter(out);
+	}
+
+	/**
+	 * Returns a new instance of {@link NQuadsWriter}.
+	 */
+	public RDFWriter getWriter(Writer writer) {
+		return new NQuadsWriter(writer);
+	}
+}
