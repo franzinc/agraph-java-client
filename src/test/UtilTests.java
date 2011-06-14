b8fa45077bf7d709329dc2c894d197e71286168c
Mike Hinchey <mhinchey@franz.com>
2011-06-13 17:13:26 -0700

    rfe10314: Support > 4b in Java events test
    Change vars that hold repository size from int to long
    to avoid overflow.
    
    Manually tested by having size() return +3b, no overflow
    situations found, nor problems with printf of longs.
    
    tests-added:   none
    tests-runs:    ant test-bigger
    release-notes: none, test code only
    performance:   only changed Events, should be negligible
    
    Change-Id: I3e392440f78cbff396a3063c354c151eac2a912a
    Reviewed-on: https://gerrit.franz.com:9080/1242
    Reviewed-by: Ahmon Dancy <dancy@franz.com>
    Tested-by: Kevin Layer <layer@franz.com>

---
+++ src/test/UtilTests.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UtilTests {
+	
+	@Test
+	public void randomLong() throws Exception {
+		Util.RandomLong r = new Util.RandomLong();
+		final long max = (((long)Integer.MAX_VALUE + 10L));
+		long near = 0;
+		long count = 0;
+		final long start = System.nanoTime();
+		while ((System.nanoTime() - start) < TimeUnit.SECONDS.toNanos(5)) {
+			count++;
+			long next = r.nextLong(max);
+			if (next >=  max) {
+				Assert.fail("next=" + next + " i=" + max);
+			}
+			if (next > (max - 10000)) {
+				near++;
+			}
+		}
+		Assert.assertTrue("count=" + count + " near=" + near, near > 10);
+	}
+	
+}
