53ca8334feed38842398288c6b6f153b925baf3e
Mikel Bancroft <mikel@franz.com>
2011-06-17 13:54:13 -0700

    Add http api for sync's and merges. Updated events test to use them.
    Add documented method optimizeIndices().
    Add undocumented methods ensureDBIdle() and forceCheckpoint().
    
    These methods are called from Monitor.stop() to force merge's and
    checkpoints, and the code doing same from the monitor.sh subprocess
    has been removed.
    
    Slightly adjusts the call sequence in the events test to close tasks
    before moving on to the next phase. Also adds a Pinger class to call
    ping() on the primary dedicated session. This is necessary to prevent
    session timeouts in the case when a phase may take longer than the
    session lifetime. A ping is sent every (sessionLifetime / 2) seconds.
    
    make test-suite run? no
    
    releases notes: non-user-visible API/code change(s)
    
    <release-note>
    optimizeIndices is now a method on AGRepositoryConnection instances.
    
    This tells the server to try and optimize the indices for this
    store. Takes a boolean wait argument, which indicates whether the HTTP
    request should return right away or whether it should wait for the
    operation to complete. Also takes an optional level argument (default
    is null, meaning use the default strategy), which determines how much
    work should be done. See the index documentation
    (http://www.franz.com/agraph/support/documentation/current/triple-index.html)
    for the meaning of the different levels.
    </release-note>
    
    Change-Id: Iba89de6bf739e742cf23a75fce4bf7324f705117

--- src/com/franz/agraph/http/AGHttpRepoClient.java
+++ src/com/franz/agraph/http/AGHttpRepoClient.java
@@ -1834,4 +1834,32 @@ public class AGHttpRepoClient implements Closeable {
 		}
 	}
 
+	public void optimizeIndices(Boolean wait, int level) throws RepositoryException {
+		String url = repoRoot + "/indices/optimize";
+		Header[] headers = {};
+		NameValuePair[] params = { new NameValuePair("wait", Boolean.toString(wait)),
+		                           new NameValuePair("level", Integer.toString(level))};
+
+		try {
+			getHTTPClient().post(url,headers,params,null,null);
+		} catch (IOException e) {
+			throw new RepositoryException(e);
+		} catch (RDFParseException e) {
+			throw new RepositoryException(e);
+		}
+	}
+
+	public void optimizeIndices(Boolean wait) throws RepositoryException {
+		String url = repoRoot + "/indices/optimize";
+		Header[] headers = {};
+		NameValuePair[] params = { new NameValuePair("wait", Boolean.toString(wait)) };
+
+		try {
+			getHTTPClient().post(url,headers,params,null,null);
+		} catch (IOException e) {
+			throw new RepositoryException(e);
+		} catch (RDFParseException e) {
+			throw new RepositoryException(e);
+		}
+	}
 }
