c42c47ecaa67c13e697fd2a0fc9a2b81e12ce4ef
Mikel Bancroft <mikel@franz.com>
2011-06-17 13:54:13 -0700

    Add http api for sync's and merges. Updated events test to use them.
    Add documented method optimizeIndices().
    Add undocumented methods ensureDBIdle() and forceCheckpoint().
    
    These methods are called from Monitor.stop() to force merge's and
    checkpoints, and the code doing same from the monitor.sh subprocess
    has been removed.
    
    Slightly adjusts the call sequence in the events test to close tasks
    before moving on to the next phase. Also adds a Pinger class to call
    ping() on the primary dedicated session. This is necessary to prevent
    session timeouts in the case when a phase may take longer than the
    session lifetime. A ping is sent every (sessionLifetime / 2) seconds.
    
    make test-suite run? no
    
    releases notes: non-user-visible API/code change(s)
    
    <release-note>
    optimizeIndices is now a method on AGRepositoryConnection instances.
    
    This tells the server to try and optimize the indices for this
    store. Takes a boolean wait argument, which indicates whether the HTTP
    request should return right away or whether it should wait for the
    operation to complete. Also takes an optional level argument (default
    is null, meaning use the default strategy), which determines how much
    work should be done. See the index documentation
    (http://www.franz.com/agraph/support/documentation/current/triple-index.html)
    for the meaning of the different levels.
    </release-note>
    
    Change-Id: Iba89de6bf739e742cf23a75fce4bf7324f705117

--- src/test/stress/monitor.sh
+++ src/test/stress/monitor.sh
@@ -6,19 +6,6 @@ if test "$1" = start; then
 elif test "$1" = end; then
     echo "monitor.sh: Ending monitoring."
     killall -USR2 nmon
-
-    catalog=$3
-    dbname=$4
-
-    [ -n "${AGRAPH_LISP_CLIENT-}" ] &&
-    [ -f /fi/cl/8.2/agraph/bin/mlisp-64 ] &&
-    echo "[" `date --rfc-3339=ns` "] Phase 0 Begin: (sync)" &&
-    /fi/cl/8.2/agraph/bin/mlisp-64 \
-	-L $AGRAPH_LISP_CLIENT/agraph4.fasl \
-	-e "(db.agraph.storage::sl-ensure-db-idle \
-               \"${dbname}\" \"${catalog}\" ${AGRAPH_PORT:-nil})" \
-	-kill &&
-    echo "[" `date --rfc-3339=ns` "] Phase 0 End: (sync)."
 else
     echo $0: unknown first argument: $1
 fi
