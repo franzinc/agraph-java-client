51521f94179b134f8e2a4627da7410d388cd5699
Bill Millar <bmillar@franz.com>
2011-07-07 12:22:28 -0700

    rfe10888: put stored procedure code in its own package
    <release-note>
    rfe10888: put stored procedure code in its own package
    
    With this change, the Java client's HTTP layer stored procedure support
    code now resides in the com.franz.agraph.http.storedproc package.
    </release-note>
    
    make prepush passes
    make javadoc runs clean
    
    Change-Id: I7cee33c0e8e6954026bc85ad50bf1cedcd701e0c

---
+++ src/com/franz/agraph/http/storedproc/SerialConstants.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+** Copyright (c) 2008-2011 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+
+package com.franz.agraph.http.storedproc;
+
+/**
+ *
+ */
+interface SerialConstants {
+
+	byte SO_VECTOR = 1;
+	byte SO_STRING = 5;
+	byte SO_NULL = 7;
+	byte SO_LIST = 8;
+	byte SO_POS_INTEGER = 9;
+	byte SO_END_OF_ITEMS = 10;
+	byte SO_NEG_INTEGER = 11;
+	byte SO_BYTEVECTOR = 15; // usb8 vector
+	
+}
