8eb481c8581250a44c903113f7a6ef749344d3e6
Bill Millar <bmillar@franz.com>
2011-07-07 12:22:28 -0700

    rfe10888: put stored procedure code in its own package
    <release-note>
    rfe10888: put stored procedure code in its own package
    
    With this change, the Java client's HTTP layer stored procedure support
    code now resides in the com.franz.agraph.http.storedproc package.
    </release-note>
    
    make prepush passes
    make javadoc runs clean
    
    Change-Id: I7cee33c0e8e6954026bc85ad50bf1cedcd701e0c
    Reviewed-on: https://gerrit.franz.com:9080/1557
    Reviewed-by: Kevin Layer <layer@franz.com>
    Tested-by: Kevin Layer <layer@franz.com>

--- src/com/franz/agraph/http/storedproc/AGEncoder.java
+++ src/com/franz/agraph/http/storedproc/AGEncoder.java
@@ -6,7 +6,7 @@
  ** http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 
-package com.franz.agraph.http;
+package com.franz.agraph.http.storedproc;
 
 import java.util.ArrayList;
 
