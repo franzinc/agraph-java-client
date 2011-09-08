1d89ca26b57679aa4eaa3c56fbd2d30bdd8221d1
Mike Hinchey <mhinchey@franz.com>
2011-09-07 21:51:33 -0700

    rfe10512: Java JNDI connection pool
    This is a new, optional feature, has no impact for
    existing users. Tested for functionality and performance
    with an uncommitted change to Events.java.
    
    tests-added:   none
    tests-run:     prepush, manual test with tomcat
    performance:   no impact
    
    <release-notes>
    rfe10512: Java JNDI connection pool
    
    Implements JNDI ObjectFactory to provide a
    connection pool to AllegroGraph server.
    
    See javadocs for package: com.franz.agraph.pool.
    </release-notes>
    
    Change-Id: Ic6d7ecaf810c270d3fbf0852346e9923b0a3f4cf

---
+++ src/com/franz/agraph/pool/package-info.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+** Copyright (c) 2008-2011 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+
+/**
+ * <p>Connection pool implementation for AllegroGraph.
+ * </p>
+ * 
+ * <p>The easiest way to use this package is with the {@link com.franz.agraph.pool.AGConnPoolJndiFactory}
+ * through JNDI configuration of your webserver.
+ * </p>
+ * 
+ * <p>The Pool library is required to use this package:
+ * <a href="http://commons.apache.org/pool/">Apache Commons Pool, commons-pool-1.5.6.jar</a>
+ * </p>
+ * 
+ * @see com.franz.agraph.pool.AGConnPoolJndiFactory
+ * 
+ * @since v4.3.3
+ */
+package com.franz.agraph.pool;
