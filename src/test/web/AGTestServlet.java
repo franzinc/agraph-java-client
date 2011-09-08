f9e5801d657757351ad35fc8125680218d31ed89
Mike Hinchey <mhinchey@franz.com>
2011-09-07 21:51:33 -0700

    rfe10512: Java JNDI connection pool
    This is a new, optional feature, has no impact for
    existing users. Tested for functionality and performance
    with an uncommitted change to Events.java.
    
    tests-added:   none
    tests-run:     prepush, manual test with tomcat
    performance:   no impact
    
    <release-notes>
    rfe10512: Java JNDI connection pool
    
    Implements JNDI ObjectFactory to provide a
    connection pool to AllegroGraph server.
    
    See javadocs for package: com.franz.agraph.pool.
    </release-notes>
    
    Change-Id: Ic6d7ecaf810c270d3fbf0852346e9923b0a3f4cf

---
+++ src/test/web/AGTestServlet.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+** Copyright (c) 2008-2011 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+
+package test.web;
+
+import java.io.IOException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.franz.agraph.pool.AGConnPool;
+import com.franz.agraph.repository.AGRepositoryConnection;
+import com.franz.util.Closer;
+
+public class AGTestServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 770497520167657818L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Closer c = new Closer();
+        try {
+            Context initCtx = c.closeLater(new InitialContext());
+            Context envCtx = (Context) c.closeLater(initCtx.lookup("java:comp/env"));
+            AGConnPool pool = (AGConnPool) envCtx.lookup("connection-pool/agraph");
+            AGRepositoryConnection conn = c.closeLater(pool.borrowConnection());
+            
+            resp.getWriter().println("size=" + conn.size());
+            resp.getWriter().flush();
+        } catch (Exception e) {
+            throw new ServletException(e);
+        } finally{
+            c.close();
+        }
+    }
+    
+}
