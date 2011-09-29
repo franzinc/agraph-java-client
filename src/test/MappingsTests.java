ab1556e61cce3f97464f7c1597782526b847eece
Bill Millar <bmillar@franz.com>
2011-09-29 07:40:35 -0700

    rfe11064: datatype mapping improvements
    <release-note>
    rfe11064: datatype mapping improvements
    
    With this change, the AGRepositoryConnection#getDatatypeMappings()
    method has been corrected to return the active datatype mappings
    for the connection, including built-in mappings for xsd datatypes.
    An option has also been added to clear all mappings including the
    built-in ones for xsd datatypes, via the AGRepositoryConnection#
    clearMappings(boolean) method.
    </release-note>
    
    Added prepush test MappingsTests.
    make prepush passes.
    make javadoc runs clean.
    
    Change-Id: I855832b7da4e89691f643d8a7f3d6a1855f77389
    Reviewed-on: https://gerrit.franz.com:9080/1739
    Reviewed-by: Kevin Layer <layer@franz.com>
    Tested-by: Kevin Layer <layer@franz.com>

---
+++ src/test/MappingsTests.java
@@ -0,0 +1,54 @@
+package test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openrdf.model.BNode;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.vocabulary.XMLSchema;
+import org.openrdf.repository.RepositoryResult;
+
+public class MappingsTests extends AGAbstractTest {
+
+	@Test
+	@Category(TestSuites.Prepush.class)
+	public void testClearMappings() throws Exception {
+		conn.clearMappings();
+		String[] mappings = conn.getDatatypeMappings();
+		int numAutoMappings = mappings.length; 
+		Assert.assertTrue("expected some automatic datatype mappings", numAutoMappings>0);
+		mappings = conn.getPredicateMappings();
+		Assert.assertEquals("expected no predicate mappings", 0, mappings.length);
+		// one way of confirming that primitive mappings are present
+		// or absent is to check for precision loss due to encoding 
+		// of the double below.
+		URI myDouble = vf.createURI("http://example.org/mydouble");
+		BNode s = vf.createBNode();
+		URI p = vf.createURI("http://example.org/hasAge");
+		String d = "1.86733E1";
+		Literal o = vf.createLiteral(d,myDouble);
+		conn.registerDatatypeMapping(myDouble, XMLSchema.DOUBLE);
+		conn.add(s, p, o);
+		RepositoryResult<Statement> results = conn.getStatements(s, p, null, false);
+		Value v = results.next().getObject();
+		Assert.assertNotSame("expected loss of precision due to encoding as a double", d, v.stringValue());
+		conn.clear();
+		Assert.assertEquals(0,conn.size());
+		conn.clearMappings(true); // removes auto-mapping of xsd primitive types
+		mappings = conn.getDatatypeMappings();
+		Assert.assertEquals("expected no mappings", 0, mappings.length);
+		// confirm xsd:double is no longer auto-mapped
+		conn.add(s, p, o);
+		results = conn.getStatements(s, p, null, false);
+		v = results.next().getObject();
+		Assert.assertEquals("expected no auto-mapping of xsd:double",d,v.stringValue());
+		conn.clearMappings(); // reestablish auto-mappings of primitive types
+		mappings = conn.getDatatypeMappings();
+		Assert.assertEquals("expected auto-mappings", numAutoMappings, mappings.length);
+	}
+
+}
