c71d6d74aaef5bfb3af614c322efe2d6bb733699
Bill Millar <bmillar@franz.com>
2012-03-06 10:03:06 -0800

    rfe11397: support RepositoryFactory and Repository configuration
    <release-note>
    rfe11397: support RepositoryFactory and Repository configuration
    
    With this change, the Java client now supports Sesame's RepositoryFactory
    api and Repository configuration from RDF data.  See javadoc for the new
    com.franz.agraph.repository.config package for details.
    </release-note>
    
    Added AGRepositoryFactoryTest to prepush tests.
    make prepush passes.
    make javadoc runs clean.
    
    Change-Id: I8b37d646c84d22822b7846994da40c6cbaeceb53

---
+++ src/com/franz/agraph/repository/config/AGRepositorySchema.java
@@ -0,0 +1,50 @@
+/******************************************************************************
+** Copyright (c) 2008-2012 Franz Inc.
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the Eclipse Public License v1.0
+** which accompanies this distribution, and is available at
+** http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+
+package com.franz.agraph.repository.config;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
+
+import com.franz.agraph.repository.AGRepository;
+
+/**
+ * Defines constants for the AGRepository schema which is used by
+ * {@link AGRepositoryFactory}s to initialize {@link AGRepository}s.
+ * 
+ */
+public class AGRepositorySchema {
+
+	/** The AGRepository schema namespace (<tt>http://franz.com/agraph/repository/config#</tt>). */
+	public static final String NAMESPACE = "http://franz.com/agraph/repository/config#";
+
+	/** <tt>http://franz.com/agraph/repository/config#serverUrl</tt> */
+	public final static URI SERVERURL;
+
+	/** <tt>http://franz.com/agraph/repository/config#username</tt> */
+	public final static URI USERNAME;
+
+	/** <tt>http://franz.com/agraph/repository/config#password</tt> */
+	public final static URI PASSWORD;
+
+	/** <tt>http://franz.com/agraph/repository/config#catalogId</tt> */
+	public final static URI CATALOGID;
+	
+	/** <tt>http://franz.com/agraph/repository/config#repositoryId</tt> */
+	public final static URI REPOSITORYID;
+	
+	static {
+		ValueFactory factory = ValueFactoryImpl.getInstance();
+		SERVERURL = factory.createURI(NAMESPACE, "serverUrl");
+		USERNAME = factory.createURI(NAMESPACE, "username");
+		PASSWORD = factory.createURI(NAMESPACE, "password");
+		CATALOGID = factory.createURI(NAMESPACE, "catalogId");
+		REPOSITORYID = factory.createURI(NAMESPACE, "repositoryId");
+	}
+}
