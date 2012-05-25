0b147525d77dbe97b194529ea0d6025b4a8c524d
Mike Hinchey <mhinchey@franz.com>
2012-05-24 23:43:11 -0700

    bug21099: java pool holds sockets open
    tests-added   : AGConnPoolClosingTest, CljTest
    tests-run     : java test-bigger
    performance   : reduce socket usage
    
    <release-notes>
    bug21099: java pool holds sockets open
    
    Problem: the AGConnPool used one MultiThreadedHttpConnectionManager
    per AGRepositoryConnection, but only closed it with the pool rather
    than with the repo. This used multiple connections to the main AGraph
    port and left http socket connections open for them and a session port
    after the AGRepositoryConnection was closed. Session ports are not
    recycled by the AGraph server if SessionPorts is not specified.
    
    Fix: use a SimpleHttpConnectionManager for each AGRepositoryConnection
    and close both at the same time. This minimizes socket connections and
    avoids the secondary pool within MultiThreadedHttpConnectionManager.
    </release-notes>
    
    Also fixes the Ant target dependencies since the jars are required and
    includes the libs.
    
    Test code now uses the clojure library.
    
    Includes a disabled test to reproduce bug21109.
    
    Test code executes netstat to check for open sockets.
    
    Changed some other tests to use java-catalog instead of root.
    
    Change-Id: I1960f8fc2eda2146b4e6f7110d6cd791f3146d81

--- src/test/pool/AGConnPoolClosingTest.java
+++ src/test/pool/AGConnPoolClosingTest.java
@@ -8,6 +8,13 @@
 
 package test.pool;
 
+import static test.Util.closeWait;
+import static test.Util.netstat;
+import static test.Util.waitFor;
+import static test.util.Clj.applyStr;
+import static test.util.Clj.filter;
+import static test.util.Clj.interpose;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -26,33 +33,42 @@ import org.slf4j.LoggerFactory;
 
 import test.AGAbstractTest;
 import test.TestSuites;
-import test.Util;
+import clojure.lang.AFn;
 
 import com.franz.agraph.pool.AGConnPool;
 import com.franz.agraph.pool.AGConnProp;
 import com.franz.agraph.pool.AGPoolProp;
+import com.franz.agraph.repository.AGCatalog;
+import com.franz.agraph.repository.AGRepository;
 import com.franz.agraph.repository.AGRepositoryConnection;
+import com.franz.agraph.repository.AGServer;
+import com.franz.util.Closer;
 
-public class AGConnPoolClosingTest extends AGAbstractTest {
+public class AGConnPoolClosingTest extends Closer {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	private static final int NUM = 10;
 
     @After
-    public void closeAfter() {
+    public void closeAfter() throws Exception {
     	close();
+    	Map<String, String> sessions = AGAbstractTest.sessions(AGAbstractTest.newAGServer());
+    	if (!sessions.isEmpty()) {
+        	log.warn("sessions after close: " + sessions);
+    	}
     }
 
     @Test
     @Category(TestSuites.Stress.class)
     public void openAG() throws Exception {
+    	final String repoName = "pool.openAG";
 		final AGConnPool pool = closeLater( AGConnPool.create(
 				AGConnProp.serverUrl, AGAbstractTest.findServerUrl(),
 				AGConnProp.username, AGAbstractTest.username(),
 				AGConnProp.password, AGAbstractTest.password(),
-				AGConnProp.catalog, "/",
-				AGConnProp.repository, "test.pool.AGConnPoolClosingTest",
+				AGConnProp.catalog, AGAbstractTest.CATALOG_ID,
+				AGConnProp.repository, repoName,
 				AGConnProp.session, AGConnProp.Session.DEDICATED,
 				AGConnProp.sessionLifetime, TimeUnit.MINUTES.toSeconds(5),
 				AGPoolProp.shutdownHook, true,
@@ -97,20 +113,113 @@ public class AGConnPoolClosingTest extends AGAbstractTest {
 
         close();
     	long start = System.nanoTime();
-        Map<String, String> procs = Util.waitFor(TimeUnit.SECONDS.toMillis(1), TimeUnit.SECONDS.toMillis(30),
-        		new Callable<Map<String, String>>() {
-        	public Map<String, String> call() throws Exception {
-        		Map<String, String> procs = processes();
-		        for (Entry<String, String> entry : procs.entrySet()) {
-					if (entry.getValue().contains("test.pool.AGConnPoolClosingTest")
-							&& entry.getValue().contains("session")) {
-						return procs;
-					}
-				}
-		        return null;
-			}
-		});
-        Assert.assertNull("Session process " + TimeUnit.NANOSECONDS.toSeconds((System.nanoTime() - start)) + " seconds after closing.", procs);
+    	final AGServer server = closeLater(AGAbstractTest.newAGServer());
+        Map<String, String> sessions = AGAbstractTest.waitForSessions(server, repoName);
+        Assert.assertNull("Sessions alive " + TimeUnit.NANOSECONDS.toSeconds((System.nanoTime() - start)) + " seconds after closing.", sessions);
+        close(server);
+    }
+
+    @Test
+    @Category(TestSuites.Stress.class)
+    public void openSockets_bug21099() throws Exception {
+        final List<String> netstatBefore = netstat();
+        {
+        	log.debug("openSockets_bug21099 netstatBefore: " + applyStr(interpose("\n", netstatBefore)));
+            List<String> closeWait = closeWait(netstatBefore);
+            if (!closeWait.isEmpty()) {
+            	log.warn("openSockets_bug21099 netstat close_wait Before: " + applyStr(interpose("\n", closeWait)));
+            }
+        }
+    	final String repoName = "pool.bug21099";
+    	AGConnPool pool = closeLater( AGConnPool.create(
+    			AGConnProp.serverUrl, AGAbstractTest.findServerUrl(),
+    			AGConnProp.username, AGAbstractTest.username(),
+    			AGConnProp.password, AGAbstractTest.password(),
+				AGConnProp.catalog, AGAbstractTest.CATALOG_ID,
+				AGConnProp.repository, repoName,
+                AGConnProp.session, AGConnProp.Session.TX,
+                AGPoolProp.maxActive, 40,
+                AGPoolProp.maxWait, 40000,
+                AGPoolProp.shutdownHook, true,
+                AGPoolProp.testOnBorrow, false,
+                AGPoolProp.minIdle, 10,
+                AGPoolProp.maxIdle, 20,
+                AGPoolProp.timeBetweenEvictionRunsMillis, 1000,
+                AGPoolProp.minEvictableIdleTimeMillis, 2000,
+                AGConnProp.sessionLifetime, 30,
+                AGPoolProp.testWhileIdle, true,
+                AGPoolProp.numTestsPerEvictionRun, 5,
+                AGPoolProp.initialSize, 5
+    	));
+        Thread.sleep(30000);
+        List<String> netstat = filter(new AFn() {
+        	public Object invoke(Object line) {
+        		return ! netstatBefore.contains(line);
+        	}
+        }, netstat());
+        List<String> closeWait = closeWait(netstat);
+        Assert.assertTrue("sockets in CLOSE_WAIT: " + applyStr(interpose("\n", closeWait)), closeWait.isEmpty());
+        Assert.assertTrue("too many sockets open: " + applyStr(interpose("\n", netstat)), 10 >= netstat.size());
+        
+        close(pool);
+        
+        netstat = AGAbstractTest.waitForNetStat(netstatBefore);
+        Assert.assertNull("sockets open after closing pool: " + netstat, netstat);
+        
+    	final AGServer server = closeLater(AGAbstractTest.newAGServer());
+    	long start = System.nanoTime();
+        Map<String, String> sessions = AGAbstractTest.waitForSessions(server, repoName);
+        Assert.assertNull("Sessions alive " + TimeUnit.NANOSECONDS.toSeconds((System.nanoTime() - start)) + " seconds after closing.", sessions);
+        close(server);
+    }
+
+    @Test
+    @Category(TestSuites.Broken.class)
+    /**
+     * Test without the pool.
+     */
+    public void openSockets_bug21109_direct() throws Exception {
+        final List<String> netstatBefore = netstat();
+    	log.debug("openSockets_bug21109_direct netstatBefore: " + applyStr(interpose("\n", netstatBefore)));
+        
+    	final String repoName = "direct.bug21109";
+    	AGServer server = closeLater(AGAbstractTest.newAGServer());
+    	AGCatalog cat = server.getCatalog(AGAbstractTest.CATALOG_ID);
+    	AGRepository repo = cat.createRepository(repoName);
+    	for (int i = 0; i < 30; i++) {
+        	AGRepositoryConnection conn1 = closeLater(repo.getConnection());
+        	conn1.setAutoCommit(false);
+        	AGRepositoryConnection conn2 = closeLater(repo.getConnection());
+        	conn2.setAutoCommit(false);
+        	AGRepositoryConnection conn3 = closeLater(repo.getConnection());
+        	conn3.setAutoCommit(false);
+        	close(conn1);
+        	close(conn2);
+        	close(conn3);
+        	if (i==5) {
+            	log.debug("netstat " + i + ": " + applyStr(interpose("\n", netstat())));
+        	}
+		}
+        List<String> netstat = filter(new AFn() {
+        	public Object invoke(Object line) {
+        		return ! netstatBefore.contains(line);
+        	}
+        }, netstat());
+        List<String> closeWait = closeWait(netstat);
+        Assert.assertTrue("sockets in CLOSE_WAIT: " + applyStr(interpose("\n", closeWait)), closeWait.isEmpty());
+        Assert.assertTrue("too many sockets open: " + applyStr(interpose("\n", netstat)), 10 >= netstat.size());
+        
+        repo = close(repo);
+        server = close(server);
+        
+        netstat = AGAbstractTest.waitForNetStat(netstatBefore);
+        Assert.assertNull("sockets open after closing pool: " + applyStr(interpose("\n", netstat)), netstat);
+        
+    	server = closeLater(AGAbstractTest.newAGServer());
+    	long start = System.nanoTime();
+        Map<String, String> sessions = AGAbstractTest.waitForSessions(server, repoName);
+        Assert.assertNull("Sessions alive " + TimeUnit.NANOSECONDS.toSeconds((System.nanoTime() - start)) + " seconds after closing.", sessions);
+        close(server);
     }
 
 }
