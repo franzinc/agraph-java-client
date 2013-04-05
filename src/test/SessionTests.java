5534724c1c810d1367e8372563a8d29b5835ea1e
Hudson Account <hudson@franz.com>
2013-04-05 14:02:26 -0700

    Increase reliability of sessionLifetime_rfe9436 test
    * Added a bunch of comments to explain how the test works.
    
    * Rearranged some code for clarity.
    
    * Added some debug output.
    
    * Increased the delay margins in the test to allow for variations in
      system load/responsiveness.
    
    * build.xml: Increase verbosity during test runs to help determine
      what's going on when test failures occur.
    
    Change-Id: I4305c0b85392e3df2e0e49c453308fd40cec8e63
    Reviewed-on: https://gerrit.franz.com:9080/3109
    Reviewed-by: Ahmon Dancy <dancy@franz.com>
    Reviewed-by: John O'Rourke <jor@franz.com>
    Tested-by: Kevin Layer <layer@franz.com>

--- src/test/SessionTests.java
+++ src/test/SessionTests.java
@@ -23,12 +23,10 @@ import com.franz.agraph.http.exception.AGHttpException;
 import com.franz.agraph.repository.AGAbstractRepository;
 import com.franz.agraph.repository.AGRepositoryConnection;
 
+import static test.Util.logTimeStamped;
+
 public class SessionTests extends AGAbstractTest {
 
-	public static int connLife = 10; 
-	public static int conn2Life = 5; 
-	public static int conn3Life = 30;
-	
 	@Test
 	@Category(TestSuites.Prepush.class)
 	public void sessionUsingDedicatedPort() throws Exception {
@@ -67,43 +65,89 @@ public class SessionTests extends AGAbstractTest {
     	URL url = new URL(conn.getHttpRepoClient().getRoot());
     	return url.getPort();
     }
-    
+
     @Test
     @Category(TestSuites.Prepush.class)
     public void sessionLifetime_rfe9436() throws Exception {
-    	Assert.assertEquals("expected default session lifetime ", AGHttpRepoClient.getDefaultSessionLifetime(),conn.getSessionLifetime());
+	// Seconds
+	int conn2Life = 5; 
+	/* This is how long past conn2Life we allow for the server to expire the session */
+	int timeoutFudge = 2;
+	/* The +10 is to allow for delays due to system load.  Don't make it too big, otherwise
+	   the test will take unnecessarily long */
+	int connLife = conn2Life + timeoutFudge + 10; 
+
+	int conn3Life = 30;
+	
+	
+	AGAbstractRepository repo = conn.getRepository();
+
+	        Assert.assertEquals("expected default session lifetime ", AGHttpRepoClient.getDefaultSessionLifetime(),conn.getSessionLifetime());
+		
 		conn.setSessionLifetime(connLife);
 		Assert.assertEquals("expected lifetime "+connLife, connLife, conn.getSessionLifetime());
-		AGAbstractRepository repo = conn.getRepository();
+
 		AGRepositoryConnection conn2 = repo.getConnection();
 		Assert.assertEquals("expected default session lifetime ", AGHttpRepoClient.getDefaultSessionLifetime(),conn2.getSessionLifetime());
 		conn2.setSessionLifetime(conn2Life);
 		Assert.assertEquals("expected lifetime "+conn2Life, conn2Life, conn2.getSessionLifetime());
+		// Make sure changes to conn2's session lifetime do not affect conn.
 		Assert.assertEquals("expected lifetime "+connLife, connLife, conn.getSessionLifetime());
+
 		AGHttpRepoClient.setDefaultSessionLifetime(conn3Life);
 		Assert.assertEquals("expected default session lifetime "+conn3Life, conn3Life, AGHttpRepoClient.getDefaultSessionLifetime());
 		AGRepositoryConnection conn3 = repo.getConnection();
 		Assert.assertEquals("expected session lifetime "+conn3Life, conn3Life, conn3.getSessionLifetime());
+		// Make sure the other connection lifetimes are still unaffected
 		Assert.assertEquals("expected lifetime "+conn2Life, conn2Life, conn2.getSessionLifetime());
 		Assert.assertEquals("expected lifetime "+connLife, connLife, conn.getSessionLifetime());
-		
+
+		logTimeStamped("conn.setAutoCommit(false);");
 		conn.setAutoCommit(false);
+		logTimeStamped("conn2.setAutoCommit(false);");
 		conn2.setAutoCommit(false);
-		Thread.sleep((conn2Life+2)*1000);
+		
+		/* Sleep past conn2's timeout.  We expect it to be expired by the time we wake */
+		int sleepTime=conn2Life+timeoutFudge;
+		logTimeStamped("Thread.sleep(" + sleepTime*1000 + ")");
+		Thread.sleep(sleepTime*1000);
 		try {
 			conn2.size();
 			Assert.fail("expected conn2 session to expire");
 		} catch (RepositoryException e) {
 		}
-		conn.ping(); // extends the life of conn's session
-		Thread.sleep((connLife-conn2Life)*1000);
+
+		// Reset conn's session timer
+		logTimeStamped("conn.ping();");
+		conn.ping(); 
+
+		/* Verify that conn's session timer was really reset.
+		   Up to this point we've already consumed at least
+		   sleepTime (conn2Life+timeoutFudge) seconds of the original
+		   timeout, leaving at most connLife-sleepTime seconds remaining. */
+
+		/* This is a maximum.  The actual original timeout remaining could
+		   be less due due to system load */
+		int orig_timeout_remaining = connLife - sleepTime;
+		
+		sleepTime = orig_timeout_remaining + timeoutFudge;
+		
+		/* Sleep past the original timeout.  */
+		logTimeStamped("Thread.sleep(" + sleepTime*1000 + ")");
+		Thread.sleep(sleepTime*1000);
+		
+		/* If the prior ping didn't really reset the session timer, this will fail */
+		logTimeStamped("conn.size();");
 		conn.size(); // fails if ping doesn't work
+
+		// Verify that closing expired conn2's session throws an exception 
 		try {
 			conn2.close();
 			Assert.fail("closing expired conn2 session should throw an exception");
 		} catch (RepositoryException e) {
 			// TODO: want a more specific exception here?
 		}
+
 		conn3.close();
 		// conn is closed in test tearDown
     }
