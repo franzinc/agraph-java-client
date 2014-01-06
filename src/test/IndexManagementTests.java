073692264e307b2ffb0b47d59e49bcb61469fc35
Ahmon Dancy <dancy@franz.com>
2014-01-06 13:51:51 -0800

    Adjusted test.IndexManagementTests.manageIndices_rfe9930() for AIMS.
    Change-Id: I439e92aa3c2722b3e6177fb38ce44c0b3474677b

--- src/test/IndexManagementTests.java
+++ src/test/IndexManagementTests.java
@@ -20,21 +20,24 @@ public class IndexManagementTests extends AGAbstractTest {
     @Test
     @Category(TestSuites.Prepush.class)
     public void manageIndices_rfe9930() throws Exception {
-	int indexCount = 7;
+	int indexCount = 1;
 		List<String> indices = conn.listValidIndices();
 		Assert.assertTrue("expected more valid indices", indices.size() >= 24);
 		indices = conn.listIndices();
 		Assert.assertEquals("unexpected number of indices initially", indexCount, indices.size());
+
+		conn.addIndex("gospi");
+		indices = conn.listIndices();
+		Assert.assertEquals("unexpected number of indices after add", (indexCount+1), indices.size());
+
+		conn.addIndex("gospi");
+		indices = conn.listIndices();
+		Assert.assertEquals("unexpected number of indices after redundant add", (indexCount+1), indices.size());
+
 		conn.dropIndex("gospi");
-		conn.dropIndex("spogi");
 		indices = conn.listIndices();
-		Assert.assertEquals("unexpected number of indices after drop", (indexCount-2), indices.size());
-		conn.addIndex("gospi");
-		indices = conn.listIndices();
-		Assert.assertEquals("unexpected number of indices after add", (indexCount-1), indices.size());
-		conn.addIndex("gospi");
-		indices = conn.listIndices();
-		Assert.assertEquals("unexpected number of indices after redundant add", (indexCount-1), indices.size());
+		Assert.assertEquals("unexpected number of indices after drop", (indexCount), indices.size());
+
     }
 
 }
