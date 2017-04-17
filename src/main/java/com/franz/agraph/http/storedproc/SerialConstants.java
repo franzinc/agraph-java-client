8ce13edf842d8c87cda7d514bd50d43879f91035
Kevin Layer <layer@franz.com>
2017-04-17 15:21:48 -0700

    Move copyrights to LICENSE file
    Change-Id: I26506fbd55114fe139cf57bd6b5bec43cda6824b
    Reviewed-on: https://gerrit.franz.com:9080/7609
    Reviewed-by: Kevin Layer <layer@franz.com>
    Reviewed-by: Tadeusz Sznuk <ts@franz.com>
    Tested-by: Hudson <hudson@franz.com>
    Reviewed-by: Ahmon Dancy <dancy@franz.com>

--- src/main/java/com/franz/agraph/http/storedproc/SerialConstants.java
+++ src/main/java/com/franz/agraph/http/storedproc/SerialConstants.java
@@ -1,9 +1,5 @@
 /******************************************************************************
-** Copyright (c) 2008-2016 Franz Inc.
-** All rights reserved. This program and the accompanying materials
-** are made available under the terms of the Eclipse Public License v1.0
-** which accompanies this distribution, and is available at
-** http://www.eclipse.org/legal/epl-v10.html
+** See the file LICENSE for the full license governing this code.
 ******************************************************************************/
 
 package com.franz.agraph.http.storedproc;
