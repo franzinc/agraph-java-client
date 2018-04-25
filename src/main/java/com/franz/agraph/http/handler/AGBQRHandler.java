5b2f9d27fe3a0b0bbe245a492a9f5ee43a427567
Ahmon Dancy <dancy@dancysoft.com>
2018-04-25 12:16:00 -0700

    added a comment.
    Change-Id: I86e4c42d162da6c511ab1aaafb7503cb6bd34e39
    Reviewed-on: https://gerrit.franz.com:9080/10637
    Tested-by: Hudson <hudson@franz.com>
    Reviewed-by: Hudson <hudson@franz.com>
    Reviewed-by: Ahmon Dancy <dancy@franz.com>

--- src/main/java/com/franz/agraph/http/handler/AGBQRHandler.java
+++ src/main/java/com/franz/agraph/http/handler/AGBQRHandler.java
@@ -14,6 +14,7 @@ import org.eclipse.rdf4j.query.resultio.QueryResultParseException;
 import java.io.IOException;
 import java.io.InputStream;
 
+// BQR == BooleanQueryResult
 public class AGBQRHandler extends AGResponseHandler {
 
     private boolean result;
