5b4018c36690a6308dced352feb6cd2080876a9b
Ihor Solodrai <solodrai@franz.com>
2024-09-14 12:33:59 -0700

    Remove unnecessary maven.compiler.release option
    Change-Id: Ic0404574a8e2493261decfc88461992028f5a6a0
    Reviewed-on: https://gerrit.franz.com:9080/c/agraph-java-client/+/26051
    Ptest: Hudson <hudson@franz.com>
    Tested-by: Hudson <hudson@franz.com>
    Reviewed-by: John Foderaro <jkf@franz.com>
    Reviewed-by: Ihor Solodrai <solodrai@franz.com>

--- events.sh
+++ events.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eu
 
-# Report Java version first
-java -version
+echo 'mvn --version in agraph-java-client/events.sh'
+
+mvn --version
 
 # Compile code (including tests) and run the main class.
 mvn test-compile exec:java -Dexec.classpathScope="test" -Dexec.args="$*" -Dexec.mainClass=test.stress.Events
